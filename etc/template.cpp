ef1a1e5b72b88ed97e8e570beceab1d7f512de93
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-04 11:44:36 -0800

    perf(architecture): add ddd

--- etc/template.cpp
+++ etc/template.cpp
@@ -1,40 +1,21 @@
-/*
-		test 
-
-
-		Created by Carlos Sanchez 
- 
-
-		Created at jue 24 sep 2020 19:25:44 PDT
-*/
- 
 
 #include <iostream>
 #include <vector>
 #include <functional>
-#include "matrix.h"
+#include "../src/shared/matrix.h"
 using namespace std;
 
 
-/*
- 	program1 
-*/
 void program1() {
 	
 }
 
 
-/*
- 	program2 
-*/
 void program2() {
 	
 }
 
 
-/*
- 	program3 
-*/
 void program3() {
 	
 }
