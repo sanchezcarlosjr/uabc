c2e375c0ec49057525456f7775eec1e5d44e919c
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-05 19:20:25 -0800

    feat(introduction-to-algorithms): add practice1-types

---
+++ src/introduction-to-algorithms/practice1-types.cpp
@@ -0,0 +1,16 @@
+/*
+        Data Types
+        Created by Carlos Sanchez
+        Created at 05/02/21
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+	
+int main() {
+    printf ("El Tipo <char> tiene un tamaño de %zd bytes\n", sizeof (char));
+    printf ("El Tipo <int> tiene un tamaño de %zd bytes\n", sizeof (int));
+    printf ("El Tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
+    printf ("El Tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
+    return 0;
+}
