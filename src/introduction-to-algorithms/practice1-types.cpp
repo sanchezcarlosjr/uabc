28f434fef965dcbd10b29f6a323687796edb18aa
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-05 19:33:20 -0800

    feat(introduction-to-algorithms): add practice1-types.cpp

--- src/introduction-to-algorithms/practice1-types.cpp
+++ src/introduction-to-algorithms/practice1-types.cpp
@@ -10,7 +10,14 @@
 int main() {
     printf ("El Tipo <char> tiene un tamaño de %zd bytes\n", sizeof (char));
     printf ("El Tipo <int> tiene un tamaño de %zd bytes\n", sizeof (int));
+    printf ("El Tipo <long> tiene un tamaño de %zd bytes\n", sizeof (long));
     printf ("El Tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
-    printf ("El Tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
+    printf ("El Tipo <unsigned short int> tiene un tamaño de %zd bytes\n", sizeof (unsigned short int)); 
+    printf ("El Tipo <long long> tiene un tamaño de %zd bytes\n", sizeof (long long));
+    printf ("El Tipo <bool> tiene un tamaño de %zd bytes\n", sizeof (bool));
+    printf ("El Tipo <unsigned char> tiene un tamaño de %zd bytes\n", sizeof (unsigned char));
+    printf ("El Tipo <double> tiene un tamaño de %zd bytes\n", sizeof (double));
+    printf ("El Tipo <long double> tiene un tamaño de %zd bytes\n", sizeof (long double));
+    printf ("El Tipo <short> tiene un tamaño de %zd bytes\n", sizeof (short));
     return 0;
 }
