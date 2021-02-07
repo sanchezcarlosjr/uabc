ea3604118184b726f74f02524c79cd0c70800d83
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-07 10:50:09 -0800

    feat(introduction-to-algorithms): add practice1 and workshop 1

--- src/introduction-to-algorithms/practice1-data-types.cpp
+++ src/introduction-to-algorithms/practice1-data-types.cpp
@@ -8,16 +8,21 @@
 #include <stdlib.h>
 	
 int main() {
-    printf ("El Tipo <char> tiene un tamaño de %zd bytes\n", sizeof (char));
-    printf ("El Tipo <int> tiene un tamaño de %zd bytes\n", sizeof (int));
-    printf ("El Tipo <long> tiene un tamaño de %zd bytes\n", sizeof (long));
-    printf ("El Tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
-    printf ("El Tipo <unsigned short int> tiene un tamaño de %zd bytes\n", sizeof (unsigned short int)); 
-    printf ("El Tipo <long long> tiene un tamaño de %zd bytes\n", sizeof (long long));
-    printf ("El Tipo <bool> tiene un tamaño de %zd bytes\n", sizeof (bool));
-    printf ("El Tipo <unsigned char> tiene un tamaño de %zd bytes\n", sizeof (unsigned char));
-    printf ("El Tipo <double> tiene un tamaño de %zd bytes\n", sizeof (double));
-    printf ("El Tipo <long double> tiene un tamaño de %zd bytes\n", sizeof (long double));
-    printf ("El Tipo <short> tiene un tamaño de %zd bytes\n", sizeof (short));
+    printf ("El tipo <char> tiene un tamaño de %zd bytes\n", sizeof (char));
+    printf ("El tipo <int> tiene un tamaño de %zd bytes\n", sizeof (int));
+    printf ("El tipo <long> tiene un tamaño de %zd bytes\n", sizeof (long));
+    printf ("El tipo <float> tiene un tamaño de %zd bytes\n", sizeof (float));
+    printf ("El tipo <short int> tiene un tamaño de %zd bytes\n", sizeof (short int));
+    printf ("El tipo <unsigned int> tiene un tamaño de %zd bytes\n", sizeof (unsigned int));
+    printf ("El tipo <short int> tiene un tamaño de %zd bytes\n", sizeof (short int));
+    printf ("El tipo <long int> tiene un tamaño de %zd bytes\n", sizeof (long int));
+    printf ("El tipo <unsigned short int> tiene un tamaño de %zd bytes\n", sizeof (unsigned short int)); 
+    printf ("El tipo <long long> tiene un tamaño de %zd bytes\n", sizeof (long long));
+    printf ("El tipo <bool> tiene un tamaño de %zd bytes\n", sizeof (bool));
+    printf ("El tipo <unsigned char> tiene un tamaño de %zd bytes\n", sizeof (unsigned char));
+    printf ("El tipo <double> tiene un tamaño de %zd bytes\n", sizeof (double));
+    printf ("El tipo <long double> tiene un tamaño de %zd bytes\n", sizeof (long double));
+    printf ("El tipo <short double> tiene un tamaño de %zd bytes\n", sizeof (short double));
+    printf ("El tipo <short> tiene un tamaño de %zd bytes\n", sizeof (short));
     return 0;
 }
