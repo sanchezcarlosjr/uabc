359654909ed55deaf649376447b480e957fc605a
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-15 16:50:25 -0800

    feat(algorithms): add square_matrix_multiply_strassen to week1-practice2

--- src/introduction-to-algorithms/week2-practice1/square_matrix_multiply_strassen.py
+++ src/introduction-to-algorithms/week2-practice1/square_matrix_multiply_strassen.py
@@ -1,5 +1,4 @@
 # Version 3.6
-
 import numpy as np
 
 
@@ -14,36 +13,30 @@ def split(matrix):
     return matrix[:row2, :col2], matrix[:row2, col2:], matrix[row2:, :col2], matrix[row2:, col2:]
 
 
-def strassen(x, y):
-    """
-    Computes matrix product by divide and conquer approach, recursively.
-    Input: nxn matrices x and y
-    Output: nxn matrix, product of x and y
-    """
-
-    # Base case when size of matrices is 1x1
-    if len(x) == 1:
-        return x * y
-
-    # Splitting the matrices into quadrants. This will be done recursively
-    # untill the base case is reached.
-    a, b, c, d = split(x)
-    e, f, g, h = split(y)
-
-    # Computing the 7 products, recursively (p1, p2...p7)
-    p1 = strassen(a, f - h)
-    p2 = strassen(a + b, h)
-    p3 = strassen(c + d, e)
-    p4 = strassen(d, g - e)
-    p5 = strassen(a + d, e + h)
-    p6 = strassen(b - d, g + h)
-    p7 = strassen(a - c, e + f)
-
-    # Computing the values of the 4 quadrants of the final matrix c
+def strassen(A, B):
+    if len(A) == 1:
+        return A * B
+    a11, a12, a21, a22 = split(A)
+    b11, b12, b21, b22 = split(B)
+    p1 = strassen(a11, b12 - b22)
+    p2 = strassen(a11 + a12, b22)
+    p3 = strassen(a21 + a22, b11)
+    p4 = strassen(a22, b21 - b11)
+    p5 = strassen(a11 + a22, b11 + b22)
+    p6 = strassen(a12 - a22, b21 + b22)
+    p7 = strassen(a11 - a21, b11 + b12)
     c11 = p5 + p4 - p2 + p6
     c12 = p1 + p2
     c21 = p3 + p4
-    c22 = p1 + p5 - p3 - p7
+    c22 = p5 + p1 - p3 - p7
+    return compress(c11, c12, c21, c22)
 
-    # Combining the 4 quadrants into a single matrix by stacking horizontally and vertically.
+
+def compress(c11, c12, c21, c22):
     return np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+
+
+A = [[1, 2], [1, 2]]
+B = [[2, 5], [5, 7]]
+# Expected C = [[12 19], [12 19]]
+print(strassen(np.matrix(A), np.matrix(B)))
