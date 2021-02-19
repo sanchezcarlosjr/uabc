d226f3d4b0480955b50a333b6fc83362aa98d9bf
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-19 12:54:00 -0800

    refactor(oop): practice4-books - move class to file

--- src/object-oriented-programming/practice4-book/main.cpp
+++ src/object-oriented-programming/practice4-book/main.cpp
@@ -4,44 +4,7 @@
         Created at 19/02/21
 */
 
-#include "Book.h"
-#include "system.h"
-#include <vector>
-
-class Library {
-private:
-    vector<Book*> library;
-public:
-    void add() {
-        string title = print<string>("Title: ");
-        int year = print<int>("Year: ");
-        this->library.push_back(new Book(title, year));
-    }
-    void borrow() {
-        int index = print<int>("Which book do you want to borrow? ");
-        if (this->library[index]->borrow()) {
-            cout << "Successful!!";
-        }
-        else {
-            cout << "Try again!";
-        }
-    }
-    void returnBook() {
-        int index = print<int>("Which book do you want to return? ");
-        this->library[index]->returnBook();
-        cout << "Successful!!";
-    }
-    void reserve() {
-        int index = print<int>("Which book do you want to reserve? ");
-        this->library[index]->reserve();
-        cout << "Successful!!";
-    }
-    void removeLast() {
-        this->library.pop_back();
-        cout << "Successful!!";
-    }
-};
-
+#include "Library.h"
 
 int main() {
     Library* library = new Library();
