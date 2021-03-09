261be2e66c2033460492089c97b29ae0ab3f58c5
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-03-09 10:31:26 -0800

    chore(algorithms-test): test2 - permute works

--- src/introduction-to-algorithms/week5-test/Permutation.cpp
+++ src/introduction-to-algorithms/week5-test/Permutation.cpp
@@ -4,8 +4,14 @@
         Created at 08/03/21
 */
 #include <iostream>
+#include "PermutationAlgorithm.h"
 using namespace std;
 
 int main() {
+    cout << permute("A");
+    cout << "\n";
+    cout << permute("AB");
+    cout << "\n";
+    cout << permute("ABC");
     return 0;
 }
