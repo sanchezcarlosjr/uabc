a434eba1b3bcc5b164ec30164ac3c0e0e62cc650
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-03-09 10:15:49 -0800

    chore(algorithms-test): test2 - permute works

---
+++ src/introduction-to-algorithms/week5-test/Permutation.cpp
@@ -0,0 +1,11 @@
+/*
+        Permutation
+        Created by Carlos Sanchez
+        Created at 08/03/21
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
--- src/introduction-to-algorithms/week5-test/Tests.cpp
+++ src/introduction-to-algorithms/week5-test/Tests.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
 #include "SumPrimesAlgorithm.h"
+#include "PermutationAlgorithm.h"
 #include "DataStructure/DataStructure.h"
 
 TEST(SumPrimesTest, itShouldCalculateSumPrimes){
     Stack<int> stack({1,2,3,4,6});
     ASSERT_EQ(5, sumPrimes(stack));
+}
+
+TEST(PermutationTest, itShouldShowPermutationWhenItIsHasMoreAElement){
+   ASSERT_EQ("", permute(""));
+   ASSERT_EQ("A\n", permute("A"));
+   ASSERT_EQ("AB\nBA\n", permute("AB"));
+   ASSERT_EQ("ABC\nACB\nBAC\nBCA\nCAB\nCBA\n", permute("ABC"));
 }
 
 int main(int argc, char **argv) {
