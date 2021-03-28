4c3bc664060f75c437c3ceab2aff3f36917be8df
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-03-28 16:41:24 -0700

    feat(lisp): remove repeated elements

---
+++ src/lisp/remdup.lisp
@@ -0,0 +1,15 @@
+(defun test (a b c)
+  (print (funcall a b))
+  (assert (equal (funcall a b) c))
+)
+(defun remdup (lst)
+  (reduce
+    	   #'(lambda (acc actual) 
+	  	(setf element (list actual))
+		(if (not (equal (last acc) element)) (append acc element) acc)
+	   ) 
+	  lst 
+	  :initial-value '()
+   )
+)
+(test #'remdup '(a a a b b c c c a a d d e e e e f) '(A B C A D E F))
