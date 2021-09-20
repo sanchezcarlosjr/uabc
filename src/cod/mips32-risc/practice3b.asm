ca7283851158596161446f9cf7a04e94d97691c4
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-09-19 19:22:51 -0700

    feat(cod): add sum of three numbers

--- src/cod/mips32-risc/practice3b.asm
+++ src/cod/mips32-risc/practice3b.asm
@@ -1,64 +1,64 @@
 #
-#  Sum two numbers R=A+B
+#  Sum three numbers R=A+B+C
 #
 .text
 	
 .globl	main
 
 main:
-        jal input
-        
-        # Print string msg2
-	li	$v0,4
-	la	$a0, msg2
-	syscall
-        
-        
-	# Print string msg2
-	li	$v0,4
-	la	$a0, msg2
-	syscall
-
-	# Get input B from user and save
-	li	$v0,5
-	syscall
-	move	$t1,$v0
-
-	# A = A + B
-	add	$t0, $t0, $t1
-
-	# Print string msg3
-	li	$v0, 4
-	la	$a0, msg3
-	syscall
-
-	# Print sum
-	li	$v0,1
-	move	$a0, $t0
-	syscall
-
-	# Print \n
-	li	$v0,4
-	la	$a0, newline
-	syscall
+    la $a1, msgA
+    jal input
+    move $t0,$v0
+    
+    la $a1, msgB
+    jal input
+    add $t0, $t0, $v0
+    
+    la $a1, msgC
+    jal input
+    add $t0, $t0, $v0
+     
+    la $a1, msgTotal
+    jal print_string
+    
+    move $a1, $t0
+    jal print_int
+    
+    la $a1, newline
+    jal print_string
 
     # exit
-	li	$v0,10
-	syscall
+    li	$v0,10
+    syscall
 
 input:
     li	$v0, 4
-    la	$a0, msg1
+    move $a0, $a1
     syscall
     
     li	$v0,5
     syscall
-    move	$t0,$v0
     
-    jr $ra 
+    jr $ra
+    
+print_string:
+    li	$v0, 4
+    move $a0, $a1
+    syscall
+    
+    jr $ra
+    
+print_int:
+    li	$v0, 1
+    move $a0, $a1
+    syscall
+    
+    jr $ra  
+  
 
 .data
-    msg1:	 .asciiz	"Enter A:   "
-    msg2:	 .asciiz	"Enter B:   "
-    msg3:	 .asciiz	"A + B = "
+    msgA:	 .asciiz	"Enter A:   "
+    msgB:	 .asciiz	"Enter B:   "
+    msgC:	 .asciiz	"Enter C:   "
+    msgTotal:	 .asciiz	"A + B + C = "
     newline: .asciiz	"\n"
