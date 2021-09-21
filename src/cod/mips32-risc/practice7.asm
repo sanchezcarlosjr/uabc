94d7532ef9792296a6e16367553c8dab367a97dc
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2021-09-20 18:51:02 -0700

    refactor(practice7): indent code

--- src/cod/mips32-risc/practice7.asm
+++ src/cod/mips32-risc/practice7.asm
@@ -1,16 +1,13 @@
-
 .text
 	li $s1, 3
 	li $s2, 2
-        li $s3, 1
-        li $s4, 1
-        
-        
+    li $s3, 1
+    li $s4, 1
+
 	bne $s3,$s4,Else
 	add $s0,$s1,$s2 # $s0 = $s1 + $s2
 	j Exit
 
-	
 # $s0 = $s1 - $s2
 Else: sub $s0,$s1,$s2
 
