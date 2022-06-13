d72ca21b1ea2d25d6c16880154b805c385bfaf74
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-06-13 10:21:37 -0700

    Update PermutationAlgorithm.h

--- src/datastructure-week5-test/PermutationAlgorithm.h
+++ src/datastructure-week5-test/PermutationAlgorithm.h
@@ -1,6 +1,8 @@
 //
 // Created by cest on 08/03/21.
 //
+// I've made an better version on
+//https://colab.research.google.com/gist/sanchezcarlosjr/d03c53576beaed6d8a6ae8b4495a9f15/introduction-to-algorithms.ipynb#scrollTo=7FkKR7TNoeOQ&line=15&uniqifier=1
 #include <string>
 using namespace std;
 
